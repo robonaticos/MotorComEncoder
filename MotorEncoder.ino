14c6735fd97ec025b9968eea1da8c2c4982d8382
Felipe Silva <felipeo.silva@hotmail.com>
2017-09-04 17:12:06 -0300

    Change counter variable name, pulses e velocity

--- MotorEncoder.ino
+++ MotorEncoder.ino
@@ -24,49 +24,48 @@
 // https://www.arduino.cc/en/Reference/AttachInterrupt
 
 Encoder encoder(18, 19);
-#define forcaMotor 150 // PWM que o motor irá girar
+#define forcaMotor 120 // PWM que o motor irá girar
 
 #define motorFrente 6 // Porta PWM que o motor está conectado na Ponte H
 #define motorTras 5 // Porta PWM que o motor está conectado na Ponte H
 
-#define pulsosPorVolta 3575 // Número de pulsos por volta que o motor lê com o encoder
+#define pulsos 720 // Número de pulsos que o motor irá girar, obs: Nos motores de LEGO 1 rotação corresponde à 720 pulsos
 
 void setup() {
   pinMode(motorFrente, OUTPUT);
   pinMode(motorTras, OUTPUT);
-  
+
   Serial.begin(9600);
   Serial.println("Inicio da contagem do Encoder:");
 }
 
-int contador = 0;
+int contador = 1;
 
 void loop() {
+
+  //Serial.println(encoder.read()); // Exibe no Serial a contagem do Encoder
   
-  Serial.println(encoder.read()); // Exibe no Serial a contagem do Encoder
-  
-  counter++;
+  delay(500);
   
   // Contador para fazer com que o motor só gire uma única vez
-  if(contador == 1) {
+  if (contador == 1) {
     // Função para movimentar o motor
     moverMotor(forcaMotor);
     // Faz com que o motor se mova até que a contagem do encoder chegue ao número determinado
-    while(encoder.read() <= pulsosPorVolta) {
+    while (encoder.read() <= pulsos) {
     }
     moverMotor(0); // quando a contagem chegar ao fim, para o motor
     Serial.print("Contagem de pulsos: ");
     Serial.println(encoder.read());
     encoder.write(0); // reseta o graus da contagem do encoder
+    contador++;
   }
-  
-  
 }
 
 /*
- * Função para movimentar o motor para frente ou para trás
- * Parâmetro é a força em que o motor irá girar
- */
+   Função para movimentar o motor para frente ou para trás
+   Parâmetro é a força em que o motor irá girar
+*/
 void moverMotor(int MotorA) {
   if (MotorA > 0) {
     analogWrite(motorFrente, MotorA);
@@ -82,7 +81,3 @@ void moverMotor(int MotorA) {
   }
 
 };
-
-
-
-
